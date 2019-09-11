6888da9e24a4d63b130dd6e89cfe9b3f638c7b07
Paulodir <paulodir.oliver@gmail.com>
2019-09-10 21:52:38 -0300

    Comentado ImageView

--- app/src/main/java/com/edu/senac/ferramentashardware/AudioFragment.java
+++ app/src/main/java/com/edu/senac/ferramentashardware/AudioFragment.java
@@ -46,7 +46,7 @@ public class AudioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-           ImageView status = view.findViewById(R.id.status);
+           //ImageView status = view.findViewById(R.id.status);
 
         // Inflate the layout for this fragment
            View view = inflater.inflate(R.layout.fragment_audio, container, false);
